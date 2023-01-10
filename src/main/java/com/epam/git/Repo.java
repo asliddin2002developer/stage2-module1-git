c9a1f737677fbd7e7034e584a19e029d8b69ead5
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2023-01-10 20:35:02 +0500

    Git Task

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/asliddin2002developer/stage2-module1-git";
+    public static String REPO_LINK = "https://github.com/asliddin2002developer/mjc-school-git-task";
 }
